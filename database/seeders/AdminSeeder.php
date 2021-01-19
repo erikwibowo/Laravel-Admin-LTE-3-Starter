b2f8a1b9c9310923ca67b4e70db60d65b02c6e2d
erikwibowo <erikwibo@gmail.com>
2021-01-19 15:43:23 +0700

    initial commit & make (migration, seeder, controller, model) admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::insert([
+            'name' => 'Erik WIbowo',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'), // password
+            'phone' => '081510815414',
+            'address' => 'Jalan Pahlawan 51161 Pekalongan Jawa Tengah',
+            'level' => "SUPER ADMIN",
+            'status' => 1,
+            'login_at' => now(),
+            'created_at' => now()
+        ]);
+    }
+}
